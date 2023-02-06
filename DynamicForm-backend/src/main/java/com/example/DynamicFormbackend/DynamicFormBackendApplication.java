6d4ff434f063d7d8d75a3451984fdf78661a70c0
Tuladhar <safala.tuladhar@cotiviti.com>
2023-02-06 11:04:02 +0545

    first push

---
+++ DynamicForm-backend/src/main/java/com/example/DynamicFormbackend/DynamicFormBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.DynamicFormbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicFormBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicFormBackendApplication.class, args);
+	}
+
+}
---
+++ DynamicForm-backend/src/test/java/com/example/DynamicFormbackend/DynamicFormBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DynamicFormbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicFormBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
