082a387ae370f4cc73493942eb80b9ac7b2d30f5
Kushal Upreti <kushalupreti101@gmail.com>
2023-02-10 14:40:49 +0545

    Added element to service on confirm

--- dynamicForm-front/src/app/enums/FormElementType.enum.ts
+++ dynamicForm-front/src/app/enums/FormElementType.enum.ts
@@ -1,23 +1,23 @@
 export enum FormElementType {
   TEXTFIELD,
-  TEXTAREA,
-  CHECKBOX,
-  FILE_UPLOAD,
   SELECT,
+  CHECKBOX,
+  TEXTAREA,
+  FILE_UPLOAD,
 }
 
 export const formElementAtrributeMap = [
-  ['multiple', 'options', 'fileType'],
-  ['multiple', 'options', 'fileType'],
-  ['multiple', 'fileType', 'placeholder'],
-  ['options', 'placeholder'],
-  ['multiple', 'fileType', 'placeholder', 'value'],
+  ['multiple', 'options', 'fileType'], //TEXTFIELD
+  ['multiple', 'fileType', 'placeholder', 'value'], //SELECT
+  ['multiple', 'fileType', 'placeholder'], //CHECKBOX
+  ['multiple', 'options', 'fileType'], //TEXTAREA
+  ['options', 'placeholder'], //FILE_UPLOAD
 ];
 
 export const formElementInfo: { class: string; title: string }[] = [
   { class: 'fa-solid fa-align-left', title: 'Text Field' },
-  { class: 'far fa-caret-square-down', title: 'Textarea' },
+  { class: 'far fa-caret-square-down', title: 'Select' },
   { class: 'fa-solid fa-square-check', title: 'Checkbox' },
-  { class: 'fa-solid fa-align-justify', title: 'File Upload' },
-  { class: 'fa-solid fa-file-arrow-up', title: 'Select' },
+  { class: 'fa-solid fa-align-justify', title: 'Textarea' },
+  { class: 'fa-solid fa-file-arrow-up', title: 'File Upload' },
 ];
