a08cc7e6eb32ae5a551681b148eb96e5019ee967
safalatuladhar <safftldr20@gmail.com>
2023-02-13 11:23:48 +0545

    html template

--- dynamicForm-front/src/app/components/home/home.component.ts
+++ dynamicForm-front/src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Form } from 'src/app/interfaces/Form.interface';
+import { FormElement } from 'src/app/interfaces/FormElement.interface';
+import { Option } from 'src/app/interfaces/Option.interface';
 
 @Component({
   selector: 'app-home',
@@ -57,29 +59,159 @@ export class HomeComponent {
 
   remove() {}
 
-
-    download() {
-       var html = `
+  download() {
+    var html = `
       <html>
         <head>
           <title>My HTML File</title>
         </head>
-        <body>
-          <h1>Hello, HTML!</h1>
-          <p>This is an HTML file.</p>
-        </body>
-      </html>
-    `;
-    
+        <body>`;
+    //       <h1>Hello, HTML!</h1>
+    //       <p>This is an HTML file.</p>
+    //     </body>
+    //   </html>
+
+    // `;
+
     const data = new Blob([html], { type: 'text/html' });
     const url = URL.createObjectURL(data);
     const link = document.createElement('a');
     link.download = 'my-file.html';
     link.href = url;
     link.click();
-    }
+  }
 
-    // fortextfield(html,formElement){
-    //   html+=`<input = "`+formElement.type+`" id =`;
-    // }
+  textField(formComponent: FormElement) {
+    const html =
+      `<div class="form-group">
+          <label for="` +
+      formComponent.id +
+      `">` +
+      formComponent.label +
+      `</label>
+          <input
+            type="text" 
+            id=` +
+      formComponent.id +
+      ` 
+            name="` +
+      formComponent.name +
+      `
+            class=" ` +
+      formComponent.class +
+      `
+            placeholder=` +
+      formComponent.placeholder +
+      `
+          />
+        </div>`;
+    return html;
+  }
+
+  option(data: Option[]) {
+    var html = '';
+    data.forEach((item) => {
+      html +=
+        ` <option value=" ` + item.name + `"> ` + item.name + `</option> `;
+    });
+
+    return html;
+  }
+
+  selectField(data: FormElement) {
+    const option = this.option(data.options);
+    const html =
+      `<div class="form-group">
+          <label for="` +
+      data.id +
+      `">` +
+      data.label +
+      `</label>
+          
+          <select
+            id=` +
+      data.id +
+      `
+            name=` +
+      data.name +
+      `
+            class=` +
+      data.class +
+      `>
+            ` +
+      option +
+      `           
+          </select>
+        </div>`;
+    return html;
+  }
+
+  checkbox(data: FormElement[]) {
+    var html = '';
+    data.forEach((item) => {
+      html += ` <value=" ` + item.name + `"> `;
+    });
+
+    return html;
+  }
+
+  checkboxField(formComponent: FormElement) {
+    const html =
+      `<div class="form-group">
+          <input  type="checkbox"  id=` +
+      formComponent.id +
+      ` 
+            name="` +
+      formComponent.name +
+      `
+            class=" ` +
+      formComponent.class +
+      this.checkbox +
+      `     />
+        <label for="` +
+      formComponent.id +
+      `">` +
+      formComponent.label +
+      `</label>
+        </div>`;
+    return html;
+  }
+
+  textareaField(formComponent: FormElement) {
+    const html =
+      `<div class="form-group">
+          <label for="` +
+      formComponent.id +
+      `">` +
+      formComponent.label +
+      `</label>
+          <textarea
+            id=` +
+      formComponent.id +
+      ` 
+            name="` +
+      formComponent.name +
+      `
+            class=" ` +
+      formComponent.class + `rows= ` + formComponent.value +
+       `cols= ` + formComponent.value + `> ` +
+        formComponent.value +`     
+          /textarea>
+        </div>`;
+    return html;
+  }
+
+  fileuploadField(formComponent: FormElement) {
+   const html =
+     `<div class="form-group">
+          <input  type="file"  id=` +
+     formComponent.id +
+     ` 
+            name="` +
+     formComponent.name +
+     `>
+       <input  type="submit">
+        </div>`;
+   return html;
+  }
 }
