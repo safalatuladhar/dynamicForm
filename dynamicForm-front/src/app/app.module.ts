c0b227ff02b8694898ed19be04e90bef763836e0
Kushal Upreti <kushalupreti101@gmail.com>
2023-02-15 11:23:03 +0545

    Fixed select disabled error Added variable enctype Added redirect button on 404 page

--- dynamicForm-front/src/app/app.module.ts
+++ dynamicForm-front/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,7 @@ import { AppComponent } from './app.component';
     ReactiveFormsModule,
     NgbModule,
     HttpClientModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- dynamicForm-front/src/app/pages/notfound-page/notfound-page.component.ts
+++ dynamicForm-front/src/app/pages/notfound-page/notfound-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-notfound-page',
   templateUrl: './notfound-page.component.html',
-  styleUrls: ['./notfound-page.component.scss']
+  styleUrls: ['./notfound-page.component.scss'],
 })
 export class NotfoundPageComponent {
+  constructor(private readonly router: Router) {}
 
+  redirect() {
+    this.router.navigate(['']);
+  }
 }
