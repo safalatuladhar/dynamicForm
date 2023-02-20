b6d5b254b0a296718d95c37750bb4e9f689356f3
safalatuladhar <safftldr20@gmail.com>
2023-02-20 14:44:51 +0545

    changed in beans

--- DynamicForm-backend/src/main/java/com/example/DynamicFormbackend/DynamicFormBackendApplication.java
+++ DynamicForm-backend/src/main/java/com/example/DynamicFormbackend/DynamicFormBackendApplication.java
@@ -3,14 +3,11 @@ package com.example.DynamicFormbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.modelmapper.ModelMapper;
+
 
 @SpringBootApplication
 public class DynamicFormBackendApplication {
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DynamicFormBackendApplication.class, args);
--- DynamicForm-backend/src/main/java/com/example/DynamicFormbackend/config/Beans.java
+++ DynamicForm-backend/src/main/java/com/example/DynamicFormbackend/config/Beans.java
@@ -2,6 +2,7 @@ package com.example.DynamicFormbackend.config;
 
 import com.example.DynamicFormbackend.Service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -16,7 +17,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+import org.modelmapper.ModelMapper;
 import java.util.HashSet;
 
 @Configuration
@@ -51,6 +52,11 @@ public class Beans {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
